4064a95542e6ea6c00b1520b6282f29c74d3a1c3
Aldawaymeh <qusay2blees@gmail.com>
2017-07-05 13:46:44 -0400

    Delete wordr.py

--- wordr.py
+++
@@ -1,7 +0,0 @@
-#read wordlist
-#نها تقوم بأخذ قائمة من النصوص وتقوم بتشفيرهم حسب ما تريد
-
-class wordlist_read: #قمنا بعمل كلاس لتقوم بقرائة قائمة النصوص
-	def list_read(self,word_list):
-		file = open(word_list, "r")
-		return  file.readlines() #لاجل اعادة عند الاستدعاء	
